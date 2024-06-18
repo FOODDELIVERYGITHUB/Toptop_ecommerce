b3fe3798a2501d776b1a7fe15ffbb9f006b05515
fooddeliverygithub <circuitfooddelivery@gmail.com>
2024-06-18 16:42:39 +0100

    firebase deployed successfully

--- src/components/common/Footer.jsx
+++ src/components/common/Footer.jsx
@@ -18,7 +18,7 @@ const Footer = () => {
           <span>
             Developed by
             {' '}
-            <a href="https://github.com/jgudo">JULIUS GUEVARRA</a>
+            <a href="">Samuel Young Anhimiah</a>
           </span>
         </strong>
       </div>
@@ -32,8 +32,8 @@ const Footer = () => {
       <div className="footer-col-3">
         <strong>
           <span>
-            Fork this project &nbsp;
-            <a href="https://github.com/jgudo/ecommerce-react">HERE</a>
+            Made with &nbsp;
+            <a href="">❤❤</a>
           </span>
         </strong>
       </div>
